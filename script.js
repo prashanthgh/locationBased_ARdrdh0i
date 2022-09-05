79f1c10392f690596306ed0a7c4eeb1ae67e5cc0
prashanthgh <34641309+prashanthgh@users.noreply.github.com>
2022-09-05 17:32:04 +0530

    Update script.js

--- script.js
+++ script.js
@@ -30,6 +30,7 @@ function renderPlaces(places) {
         model.setAttribute('rotation', '0 180 0');
         model.setAttribute('animation-mixer', '');
         model.setAttribute('scale', '0.5 0.5 0.5');
+         model.setAttribute('position', '0 0 0');
 
         model.addEventListener('loaded', () => {
             window.dispatchEvent(new CustomEvent('gps-entity-place-loaded'))
