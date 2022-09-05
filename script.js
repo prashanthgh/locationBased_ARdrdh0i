f2fc4626e68418a24c4f57a6eafc7c715adbd67a
prashanthgh <34641309+prashanthgh@users.noreply.github.com>
2022-09-05 13:25:55 +0530

    Update script.js

--- script.js
+++ script.js
@@ -22,17 +22,27 @@ function renderPlaces(places) {
         let latitude = place.location.lat;
         let longitude = place.location.lng;
 
+        
+         
         let model = document.createElement('a-entity');
         model.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
         model.setAttribute('gltf-model', 'https://raw.githack.com/AR-js-org/AR.js/master/aframe/examples/image-tracking/nft/trex/scene.gltf');
         model.setAttribute('rotation', '0 180 0');
         model.setAttribute('animation-mixer', '');
         model.setAttribute('scale', '0.5 0.5 0.5');
+         
+         let model2 = document.createElement('a-image');
+        model.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
+        model.setAttribute('src', 'https://www.slashgear.com/wp-content/uploads/2012/05/CityLensCameraView_final.jpg');
+        model.setAttribute('rotation', '-90 0');
+        model.setAttribute('width', '5');
+        model.setAttribute('height', '5');
 
         model.addEventListener('loaded', () => {
             window.dispatchEvent(new CustomEvent('gps-entity-place-loaded'))
         });
 
         scene.appendChild(model);
+        scene.appendChild(model2);
     });
 }
